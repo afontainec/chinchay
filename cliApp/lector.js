cfaed5777b274912ac433da4dde11de14f679bf6
Antonio Fontaine <afontaine@uc.cl>
2018-11-13 08:53:40 -0300

    include version

--- cliApp/lector.js
+++ cliApp/lector.js
@@ -4,8 +4,12 @@
 const program = require('commander');
 const intrepeter = require('../cliApp/intrepeter');
 
+const fs = require('fs');
+
+const content = fs.readFileSync('./package.json').toString();
+
 program
-  .version('0.0.1');
+  .version(JSON.parse(content).version);
 
 
 program
@@ -17,39 +21,32 @@ program
     intrepeter.new(table_name, options);
   });
 
-program
-  .command('qualify')
-  .alias('q')
-  .description('get all the repositories of a particular organization')
-  .option('-n, --number [n]', 'number of times to simulate')
-  .option('-c, --country [country]', 'country to qualify')
-  .option('-r, --round [round]', 'round from which start to simulate the matches')
-  .option('-p, --position [position]', 'position from to which it has to get to qualify')
-  .option('-m, --mode [mode]', 'mode to select match to pull down')
-  .action(() => {
-    console.log('qualify');
-  });
-
-program
-    .command('simulate')
-    .alias('s')
-    .description('get all the repositories of a particular organization')
-    .option('-n, --number [n]', 'number of times to simulate')
-    .option('-r, --round [round]', 'round from which start to simulate the matches')
-    .action(() => {
-    });
+// program
+//   .command('qualify')
+//   .alias('q')
+//   .description('get all the repositories of a particular organization')
+//   .option('-n, --number [n]', 'number of times to simulate')
+//   .option('-c, --country [country]', 'country to qualify')
+//   .option('-r, --round [round]', 'round from which start to simulate the matches')
+//   .option('-p, --position [position]', 'position from to which it has to get to qualify')
+//   .option('-m, --mode [mode]', 'mode to select match to pull down')
+//   .action(() => {
+//     console.log('qualify');
+//   });
+//
+// program
+//     .command('simulate')
+//     .alias('s')
+//     .description('get all the repositories of a particular organization')
+//     .option('-n, --number [n]', 'number of times to simulate')
+//     .option('-r, --round [round]', 'round from which start to simulate the matches')
+//     .action(() => {
+//     });
 
 
 program
   .on('--help', () => {
-    console.log('  For the RF requested:');
-    console.log();
-    console.log(' RF2:');
-    console.log('    $ saffie repositories github -o rails');
-    console.log(' RF3:');
-    console.log('    $ saffie has-repository github -o rails');
-    console.log('    $ saffie last-commits github -o github');
-    console.log();
+    console.log('  For help visit https://afontainec.github.io/chinchay/');
   });
 
 program
