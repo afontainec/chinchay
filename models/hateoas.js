ffb11220cba4b9960311b983aa1edd572719b633
Antonio Fontaine <afontaine@uc.cl>
2018-11-20 08:44:31 -0300

    reorder

--- models/hateoas.js
+++ models/hateoas.js
@@ -1,4 +1,3 @@
-const DEFAULT_URIS = {};
 
 class HATEOAS {
   constructor() {
--- test/tablegateway/mergeRawSelect.js
+++ test/tablegateway/mergeRawSelect.js
@@ -2,8 +2,8 @@
 process.env.NODE_ENV = 'test';
 
 // Require the dev-dependencies
-const chai = require('chai');
-const Table = require('..').Table;
+const chai = require('chai');// eslint-disable-line
+const Table = require('../..').Table;
 
 
 const assert = chai.assert; //eslint-disable-line
