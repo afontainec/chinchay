6bda30b02b64fb29b361acaff9ae96c390c019ff
Antonio Fontaine <afontaine@uc.cl>
2019-03-29 08:58:57 -0300

    reorder tests

---
+++ test/integrationTests/tablegateway/save/save.js
@@ -0,0 +1,47 @@
+// During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+
+// Require the dev-dependencies
+const chai = require('chai'); // eslint-disable-line
+const knex = require('../../../../knex');
+const Coffee = require('../../../../models/coffee-example');
+const Table = require('../../../../models/table');
+
+
+const assert = chai.assert; //eslint-disable-line
+
+
+// Our parent block
+describe('TABLE GATEWAY: new', () => { // eslint-disable-line
+  before(async () => { // eslint-disable-line
+    await knex.seed.run();
+  });
+
+  it('Get instance', async () => { // eslint-disable-line
+    const entry = await Coffee.new();
+    const expected = {
+      id: null,
+      name: null,
+      price: null,
+      created_at: null,
+      updated_at: null,
+    };
+    assert.deepEqual(entry, expected);
+  });
+
+  describe('unexistant table', () => { // eslint-disable-line
+    before(async () => { // eslint-disable-line
+      await knex.seed.run();
+    });
+
+    it('Get instance', (done) => { // eslint-disable-line
+      const tableName = 'does_not_exist';
+      const Unexistant = new Table(tableName);
+      Unexistant.new().then(() => {
+        done(new Error('Should not get here'));
+      }).catch(() => {
+        done();
+      });
+    });
+  });
+});
