0ea857fa026758b72e9aecdcce1ba9c4a1a305b6
Diego Bordeu <dbordeu@uc.cl>
2020-02-04 17:33:50 -0300

    reboot

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const path = require('path');
 const fs = require('fs');
 const Table = require('./models/table');
 const Hateoas = require('./models/hateoas');
+const Access = require('./models/access');
 
 
 const chainConfig = getConfig();
 
 const knex = require(chainConfig.knex); // eslint-disable-line import/no-dynamic-require
+const thewall = require(chainConfig.thewall);
 
 
 function getConfig() {
