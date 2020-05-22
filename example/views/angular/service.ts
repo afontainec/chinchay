1f1c98b3b4d74d5ae08a1ee5f18538275e5ef3fb
antonio <afontaine@uc.cl>
2020-05-22 16:38:27 -0400

    minor fixes in service

--- example/views/angular/service.ts
+++ example/views/angular/service.ts
@@ -5,51 +5,52 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class $TABLE_NAME$Service {
+export class $MODELNAME$Service {
 
   private backend: string = environment.backend;
+  private BASE_PATH: string;
 
   constructor(private http: HttpClient) { 
     this.BASE_PATH = `${this.backend}/api/$TABLE_NAME$`;
   }
 
   async new(user) {
-    const request: any = await this.http.post(`${BASE_PATH}/new`, user).toPromise();
+    const request: any = await this.http.post(`${this.BASE_PATH}/new`, user).toPromise();
     return request.data; 
   }
 
   async template() {
-    const request: any = await this.http.get(`${BASE_PATH}/template`).toPromise();
+    const request: any = await this.http.get(`${this.BASE_PATH}/template`).toPromise();
     return request.data; 
   }
 
   async find(search) {
-    const request: any = await this.http.get(`${BASE_PATH}/find`, { params: search }).toPromise();
+    const request: any = await this.http.get(`${this.BASE_PATH}/find`, { params: search }).toPromise();
     return request.data; 
   }
 
   async count() {
-    const request: any = await this.http.get(`${BASE_PATH}/count`).toPromise();
+    const request: any = await this.http.get(`${this.BASE_PATH}/count`).toPromise();
     return request.data; 
   }
 
   async findById(id) {
-    const request: any = await this.http.get(`${BASE_PATH}/${id}`).toPromise();
+    const request: any = await this.http.get(`${this.BASE_PATH}/${id}`).toPromise();
     return request.data; 
   }
 
   async all() {
-    return find();
+    return this.find({});
   }
 
 
   async update(id, values) {
-    const request: any = await this.http.put(`${BASE_PATH}/${id}/edit`, values).toPromise();
+    const request: any = await this.http.put(`${this.BASE_PATH}/${id}/edit`, values).toPromise();
     return request.data; 
   }
 
   async delete(id) {
-    const request: any = await this.http.delete(`${BASE_PATH}/${id}`).toPromise();
+    const request: any = await this.http.delete(`${this.BASE_PATH}/${id}`).toPromise();
     return request.data; 
   }
 
