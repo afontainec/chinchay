296424cd55cd514c84607d3edaad7ddb091901d6
antonio <afontaine@uc.cl>
2020-05-22 15:27:22 -0400

    get Service Path

--- cliApp/views/angular.js
+++ cliApp/views/angular.js
@@ -1,5 +1,5 @@
 const { execSync } = require('child_process');
-
+const path = require('path');
 
 const createFile = async (tableName, values, config) => {
   const APP_PATH = getAngularAppPath(config);
@@ -20,13 +20,32 @@ const createFile = async (tableName, values, config) => {
 const buildService = (tableName, values, config, APP_PATH) => {
   const command = ngGenerateService(values, APP_PATH);
   const result = execSync(command).toString();
-  console.log(result);
+  const servicePath = getServicePath(APP_PATH, result);
+  console.log(servicePath);
 };
 
 const ngGenerateService = (values, APP_PATH) => {
   const MODEL = values.MODELFILENAME;
   const command = `cd '${APP_PATH}' && ng generate service '${MODEL}/${MODEL}Service/${MODEL}'`;
   return command;
+};
+
+const getServicePath = (APP_PATH, result) => {
+  const lines = result.split(/\r?\n/);
+  const tsFile = getTSFile(lines);
+  return path.join(APP_PATH, tsFile);
+
+};
+
+const getTSFile = (lines) => {
+  for (let i = 0; i < lines.length; i++) {
+    const element = lines[i];
+    if (element.includes('.ts') && !element.includes('.spec.ts')) {
+      const pieces = element.split(' ');
+      return pieces[1];
+    }
+  }
+  throw new Error(`Could not create angular: missing TS FILE in ${lines}`);
 };
 
 
