118ef0ffac7e3cc8af6f162eabc3817bc25f4321
antonio <afontaine@uc.cl>
2020-08-15 18:55:07 -0400

    test(extractOptions): tests for getSecurityMode

--- test/models/table/static/getSecurityMode.js
+++ test/models/table/static/getSecurityMode.js
@@ -1,4 +1,4 @@
-// During the test the env variable is set to test
+/* global describe, it */
 process.env.NODE_ENV = 'test';
 
 // Require the dev-dependencies
@@ -8,109 +8,37 @@ const Utils = require('codemaster').utils;
 const { Table } = require('../../../..');
 
 
-const query = {
-  price: '40',
-  groupBy: 'name',
-  orderBy: 'order',
-  startDate: '2018-01-01',
-  endDate: '2018-01-01',
-  limit: 10,
-  offset: 1,
-  rawSelect: 'select',
-  clearSelect: true,
-};
+// eslint-disable-next-line max-lines-per-function
+describe('TABLE GATEWAY: getSecurityMode', () => {
 
-const secureOptions = Utils.cloneJSON(query);
-delete secureOptions.price;
-delete secureOptions.rawSelect;
-
-
-// Our parent block
-describe('TABLE GATEWAY: extractOptions', () => { // eslint-disable-line
-  it('securityMode is undefined ', (done) => { // eslint-disable-line
-    const input = Utils.cloneJSON(query);
-    const extracted = Table.extractOptions(input);
-    assert.deepEqual(extracted, secureOptions);
-    assert.equal(Object.keys(input).length, 1);
-    assert.equal(input.price, 40);
+  it('configuration = true (deprecated version) ', (done) => {
+    const securityMode = Table.getSecurityMode(true);
+    assert.isTrue(securityMode);
     done();
   });
 
-  it('Extract rawWhere', (done) => { // eslint-disable-line
-    const q = {
-      name: 'some',
-      rawWhere: 'price = 60 or price = 40',
-    };
-    const extracted = Table.extractOptions(Utils.cloneJSON(q), { securityMode: false });
-    assert.equal(Object.keys(extracted).length, 1);
-    assert.exists(extracted.rawWhere);
-    assert.equal(q.rawWhere, extracted.rawWhere);
+  it('configuration is undefined', (done) => {
+    const securityMode = Table.getSecurityMode();
+    assert.isTrue(securityMode);
     done();
   });
 
-  it('Extract rawWhere as array', (done) => { // eslint-disable-line
-    const q = {
-      name: 'some',
-      rawWhere: '["price = ? or price = 40", 6]',
-    };
-    const extracted = Table.extractOptions(Utils.cloneJSON(q), { securityMode: false });
-    assert.equal(Object.keys(extracted).length, 1);
-    assert.exists(extracted.rawWhere);
-    assert.isArray(extracted.rawWhere);
-    assert.equal(extracted.rawWhere[0], 'price = ? or price = 40');
-    assert.equal(extracted.rawWhere[1], 6);
+  it('configuration.securityMode = true', (done) => {
+    const securityMode = Table.getSecurityMode({ securityMode: true });
+    assert.isTrue(securityMode);
     done();
   });
 
-  describe('security mode', () => { // eslint-disable-line
-    it('No raw given ', (done) => { // eslint-disable-line
-      const input = Utils.cloneJSON(query);
-      delete input.rawSelect;
-      const expected = Utils.cloneJSON(input);
-      delete expected.price;
-      const extracted = Table.extractOptions(input, true);
-      assert.deepEqual(extracted, expected);
-      assert.equal(Object.keys(input).length, 1);
-      assert.equal(input.price, 40);
-      done();
-    });
-
-    it('With raw where and raw select ', (done) => { // eslint-disable-line
-      const input = Utils.cloneJSON(query);
-      input.rawWhere = 'raw where';
-      const expected = Utils.cloneJSON(input);
-      delete expected.price;
-      delete expected.rawWhere;
-      delete expected.rawSelect;
-      const extracted = Table.extractOptions(input, true);
-      assert.deepEqual(extracted, expected);
-      assert.equal(Object.keys(input).length, 1);
-      assert.equal(input.price, 40);
-      done();
-    });
-
-    it('With raw where and raw select, configuration: securityMode:true ', (done) => { // eslint-disable-line
-      const input = Utils.cloneJSON(query);
-      input.rawWhere = 'raw where';
-      const expected = Utils.cloneJSON(input);
-      delete expected.price;
-      delete expected.rawWhere;
-      delete expected.rawSelect;
-      const extracted = Table.extractOptions(input, { securityMode: true });
-      assert.deepEqual(extracted, expected);
-      assert.equal(Object.keys(input).length, 1);
-      assert.equal(input.price, 40);
-      done();
-    });
-  });
-
-  it('extract countDistinct ', (done) => { // eslint-disable-line
-    const input = { countDistinct: 'id', other: 'other' };
-    const expected = { countDistinct: 'id' };
-    const extracted = Table.extractOptions(input, true);
-    assert.deepEqual(extracted, expected);
-    assert.equal(Object.keys(input).length, 1);
-    assert.equal(input.other, 'other');
+  it('configuration.securityMode = undefined', (done) => {
+    const securityMode = Table.getSecurityMode({ });
+    assert.isTrue(securityMode);
     done();
   });
+
+  it('configuration.securityMode = false', (done) => {
+    const securityMode = Table.getSecurityMode({ securityMode: false });
+    assert.isFalse(securityMode);
+    done();
+  });
+
 });
