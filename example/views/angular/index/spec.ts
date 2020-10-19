21664c9fce15e99c8bf0cbfe39800dc68e03fdff
antonio <afontaine@uc.cl>
2020-10-19 15:31:26 -0300

    feat(testForComponents): pass tests for all components

--- example/views/angular/index/spec.ts
+++ example/views/angular/index/spec.ts
@@ -1,7 +1,8 @@
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 import { Index$PASCAL_CASE$Component } from './index-$KEBAB_CASE$.component';
 
@@ -12,7 +13,7 @@ describe('Index$PASCAL_CASE$ComponentComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ HttpClientTestingModule, RouterModule ],
+      imports: [ HttpClientTestingModule, RouterTestingModule, FormsModule ],
       declarations: [ Index$PASCAL_CASE$Component ]
     })
     .compileComponents();
--- example/views/angular/show/spec.ts
+++ example/views/angular/show/spec.ts
@@ -1,1 +1,33 @@
 
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+
+import { Show$PASCAL_CASE$Component } from './show-$KEBAB_CASE$.component';
+
+describe('Show$PASCAL_CASE$ComponentComponent', () => {
+  let httpClient: HttpClient;
+  let component: Show$PASCAL_CASE$Component;
+  let fixture: ComponentFixture<Show$PASCAL_CASE$Component>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule, RouterTestingModule, FormsModule ],
+      declarations: [ Show$PASCAL_CASE$Component ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    httpClient = TestBed.get(HttpClient);
+    fixture = TestBed.createComponent(Show$PASCAL_CASE$Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
