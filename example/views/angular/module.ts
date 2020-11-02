264cdfad9d4df353eb8355d404169e96cc8e4ef7
antonio <afontaine@uc.cl>
2020-11-02 17:20:21 -0300

    fix: add form and httpclient to module

--- example/views/angular/module.ts
+++ example/views/angular/module.ts
@@ -1,3 +1,5 @@
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { $PASCAL_CASE$RoutingModule } from './$KEBAB_CASE$-routing.module';
@@ -7,6 +9,8 @@ import { $PASCAL_CASE$RoutingModule } from './$KEBAB_CASE$-routing.module';
 @NgModule({
   declarations: [],
   imports: [
+    FormsModule,
+    HttpClientModule,
     CommonModule,
     $PASCAL_CASE$RoutingModule,
   ]
