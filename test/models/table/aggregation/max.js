78a44499aab121d54fa280a8d326675cb45ef7a9
afontainec <afontaine@uc.cl>
2021-03-01 17:56:59 -0300

    test(aggregation): min and max

---
+++ test/models/table/aggregation/max.js
@@ -0,0 +1,24 @@
+/* global describe, it, before */
+process.env.NODE_ENV = 'test';
+
+// Require the dev-dependencies
+const { assert } = require('chai');
+const knex = require('../../../../knex');
+const Coffee = require('../../../../models/coffee-example');
+
+
+// Our parent block
+describe('TABLE GATEWAY: max', () => { // eslint-disable-line max-lines-per-function
+
+  before(async () => {
+    await knex.seed.run();
+  });
+
+  it('happy path', async () => {
+    const column = 'price';
+    const search = { price: ['>', 90] };
+    const options = { };
+    const result = await Coffee.max(column, search, options);
+    assert.equal(result, 110);
+  });
+});
